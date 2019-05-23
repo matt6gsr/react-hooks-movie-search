da19d61b8effc211b8ec064e0e9be20c1ac9863e
matt6gsr <matt6gsr@gmail.com>
2019-05-23 14:58:58 +0100

    config remove from gitignore file

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const MY_API_KEY = 'apikey=a2215d40';
+export const BASE_URL = 'https://www.omdbapi.com/?s=';
+export const MOVIE_API_URL = `https://www.omdbapi.com/?s=man&${MY_API_KEY}`;
+export const IMDB_URL = 'https://www.imdb.com/title/';
